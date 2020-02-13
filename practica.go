9b239b0975a0fcfc9b3a097113b6b9dbea8562b2
Wojtec <poninski.w@gmail.com>
2020-02-13 11:58:57 +0100

    view image working

--- practica.go
+++ practica.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 //type struct
@@ -18,9 +20,36 @@ func main() {
 	http.HandleFunc("/", htmlHandler)
 	http.HandleFunc("/xml", xmlHandler)
 	http.HandleFunc("/json", jsonHandler)
+	http.HandleFunc("/plain", plainHandler)
+	http.HandleFunc("/image", imageHandler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 
 }
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	img, err := os.Open("scooby.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer img.Close()
+	w.Header().Set("Content-type", "image/jpeg; charset=utf8")
+	w.WriteHeader(http.StatusOK)
+	io.Copy(w, img)
+}
+
+func plainHandler(w http.ResponseWriter, r *http.Request) {
+	profile := Profile{"Wojtek", []string{"Travels", "Programming"}}
+
+	plain, err := json.MarshalIndent(profile, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-type", "text/plain; charset=utf8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(plain)
+
+}
+
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
 
 	profile := Profile{"Wojtek", []string{"Travels", "Programming"}}
@@ -30,6 +59,8 @@ func jsonHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	w.Header().Set("Content-type", "application/json; charset=utf8")
+	w.WriteHeader(http.StatusOK)
 	w.Write(js)
 }
 func xmlHandler(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +84,11 @@ func htmlHandler(w http.ResponseWriter, r *http.Request) {
 		<head><title>Simple server</title></head>
 		<body>
 		<h1>Hello </h1>
-		<p>First start website in go</p>
+		<p>Write endpoint for each action what you wanna see :</p>
+		<p> JSON write endpoint /json</p>
+		<p> XML write endpoint /xml</p>
+		<p> Plain text write endpoint /plain </p>
+		<p> Image write endpoint /image</p>
 		<body>
 		</html>
 		`))
